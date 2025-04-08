458b62f57af719e567b3bf6546b23dbcb7adf104
Ramez Shatra <ramezshatra7@gmail.com>
2025-04-08 17:09:49 +0200

    Comments were added to explain some new concepts

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -28,3 +28,17 @@ exec mysqld
 
 # Debug: Log completion of script
 echo "$(date) - Database creation script completed."
+
+
+# 9 Initialize the MariaDB database with specified user and data directory
+# 27 Start the MariaDB server
+
+
+# we made DB_USER in mariadb service, user in any database is essential for several reasons:
+# - User accounts help control who can access the database and what they can do
+# - When a user connects to MariaDB, the system authenticates them using a username and password
+	# This helps ensure that only the intended person or application can access the database.
+# -By creating separate users for different roles or applications (e.g., one user for WordPress and
+	# another for backup purposes), you can limit what each user can do. This is crucial for maintaining
+	# a secure environment where sensitive data is protected.
+# and more like: Database Ownership, Application-Specific Access (wordpress in our case), Audit and Monitoring
